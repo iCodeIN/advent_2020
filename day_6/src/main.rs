b27b6d4af8a4eda5e3a4918d66ca1163c47556fa
joe.wright <joe@joejag.com>
2020-12-08 18:51:21 +0000

    Day 6: misunderstood requirement

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,27 +1,45 @@
 #[allow(unused_imports)]
 use indoc::indoc;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     println!("Part One: {}", part_one());
+    println!("Part Two: {}", part_two());
 }
 
 fn part_one() -> usize {
     let input = fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
     let group_answers: Vec<String> = join_lines_between_blank_lines(&input);
-    let scores = calculate_scores_per_group(group_answers);
-    scores.into_iter().sum()
-}
-
-fn calculate_scores_per_group(group_answers: Vec<String>) -> Vec<usize> {
     group_answers
         .into_iter()
-        .map(|group| {
-            let unique: HashSet<char> = group.chars().collect();
-            unique.len()
-        })
-        .collect()
+        .map(|group| unique_letters(&group))
+        .sum()
+}
+
+fn part_two() -> usize {
+    let input = fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
+    let group_answers: Vec<String> = join_lines_between_blank_lines(&input);
+    group_answers
+        .into_iter()
+        .map(|group| repeated_letters(&group).len())
+        .sum()
+}
+
+fn unique_letters(input: &str) -> usize {
+    let unique: HashSet<char> = input.chars().collect();
+    unique.len()
+}
+
+fn repeated_letters(input: &str) -> Vec<char> {
+    let mut frequency: HashMap<char, u32> = HashMap::new();
+    for letter in input.chars() {
+        *frequency.entry(letter).or_insert(0) += 1;
+    }
+
+    let repeated_only: HashMap<char, u32> = frequency.into_iter().filter(|&(_, v)| v > 1).collect();
+    repeated_only.keys().cloned().collect()
 }
 
 fn join_lines_between_blank_lines(input: &str) -> Vec<String> {
@@ -29,6 +47,19 @@ fn join_lines_between_blank_lines(input: &str) -> Vec<String> {
         .split("\n\n")
         .map(|l| l.chars().filter(|c| !c.is_whitespace()).collect())
         .collect()
+}
+
+#[test]
+fn can_work_out_unique() {
+    assert_eq!(3, unique_letters("abc"));
+    assert_eq!(3, unique_letters("abac"));
+    assert_eq!(1, unique_letters("aaaa"));
+    assert_eq!(1, unique_letters("b"));
+}
+
+#[test]
+fn can_work_out_repeated() {
+    assert_eq!(vec!['a'], repeated_letters("aabc"));
 }
 
 #[test]
@@ -51,17 +82,4 @@ fn it_tidies_up_input() {
     let lines = join_lines_between_blank_lines(&input);
 
     assert_eq!(lines, vec!["abc", "abc", "abac", "aaaa", "b"]);
-}
-
-#[test]
-fn it_calculates() {
-    let answers = calculate_scores_per_group(vec![
-        "abc".to_string(),
-        "abc".to_string(),
-        "abac".to_string(),
-        "aaaa".to_string(),
-        "b".to_string(),
-    ]);
-
-    assert_eq!(answers, vec![3, 3, 3, 1, 1]);
 }
