41941c9545f9cae46e621519b3621e9b35738a9f
joe.wright <joe@joejag.com>
2020-12-09 22:36:03 +0000

    Day 7: Part 1: Find parents

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,5 +1,8 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 lazy_static! {
     static ref BAGS_REGEX: Regex = Regex::new(r"(\w* \w*) bags?").unwrap();
@@ -10,13 +13,27 @@ fn main() {
 }
 
 fn find_bags(input: &str) -> Vec<&str> {
-    let a: Vec<_> = BAGS_REGEX
+    BAGS_REGEX
         .captures_iter(input)
         .into_iter()
         .map(|c| c.get(1).map_or("", |m| m.as_str()))
-        .collect();
+        .collect()
+}
 
-    a
+fn find_bag_parents(input: Vec<Vec<&str>>) -> HashMap<&str, HashSet<&str>> {
+    let mut result: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for line in input {
+        let parent = line.first().unwrap_or(&"");
+        let children: &[&str] = &line[1..];
+
+        for child in children {
+            let known_kids = result.entry(child).or_insert(HashSet::new());
+            known_kids.insert(parent);
+        }
+    }
+
+    result
 }
 
 #[test]
@@ -26,4 +43,25 @@ fn can_find_bags() {
         vec!["light red", "bright white", "muted yellow"],
         find_bags(input)
     );
+}
+
+#[test]
+fn can_find_bag_parents() {
+    let input = vec![
+        vec!["light red", "bright white", "muted yellow"],
+        vec!["dark orange", "bright white", "muted yellow"],
+        vec!["bright white", "shiny gold"],
+    ];
+    let mut expected: HashMap<&str, HashSet<&str>> = HashMap::new();
+    expected.insert(
+        "bright white",
+        HashSet::from_iter(vec!["light red", "dark orange"]),
+    );
+    expected.insert(
+        "muted yellow",
+        HashSet::from_iter(vec!["light red", "dark orange"]),
+    );
+    expected.insert("shiny gold", HashSet::from_iter(vec!["bright white"]));
+
+    assert_eq!(expected, find_bag_parents(input))
 }
